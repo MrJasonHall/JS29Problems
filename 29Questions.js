60a94c5c0e52f0f6cfbd3396637d0bde5c22f8dd
MrJasonHall <15315028+MrJasonHall@users.noreply.github.com>
2019-04-15 22:27:13 +0100

    3rd Exercise Complete

--- 29Questions.js
+++ 29Questions.js
@@ -36,5 +36,17 @@ console.log(factorialize(-5));
 console.log(factorialize(0));
 console.log(factorialize(10));
 
+//Longest word in a sentence
 
+function longestWord(sen) {
+    //remove all non letters and split after spaces
+    var long = sen.replace(/[^a-zA-Z ]/g,"").split(" ");
+    //Use reduce to find the longest word
+    var longest = long.reduce((a, b) => a.length > b.length ? a : b, '');
+    return longest;
 
+}
+
+console.log(longestWord('Hello everyone have a happy day!'));
+console.log(longestWord('The quick brown fox jumped over the lazy dog'));
+console.log(longestWord('What is the average airspeed velocity of an unladen swallow'));
